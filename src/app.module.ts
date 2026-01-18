177b9ff70631d6cb8b0f50319c8098162e1e9881
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-01-18 10:46:04 +0200

    chore: setup mongodb using docker and connect application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import {Module} from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.getOrThrow('MONGODB_URI'),
+            }),
+            inject: [ConfigService]
+        }),
+    ],
     controllers: [],
     providers: [],
 })
