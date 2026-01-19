f8a8d81f1a475ef894428f558b921b2b40de86c2
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-01-19 21:12:39 +0200

    refactor: change findRoleByName to find-or-create behavior

--- src/modules/roles/roles.repository.ts
+++ src/modules/roles/roles.repository.ts
@@ -9,15 +9,19 @@ export class RolesRepository {
     constructor(@InjectModel(Roles.name) private readonly rolesModel: Model<Roles>) {
     }
 
-    async createRole(role: Roles) {
-        return this.rolesModel.create(role);
+    async createRole(roleName: string) {
+        return this.rolesModel.create({name: roleName});
     }
 
     async findAll() {
         return this.rolesModel.find({}, {__v: false});
     }
 
-    async findRoleByName(name: string) {
-        return this.rolesModel.findOne({name}, {__v: false});
+    async findRoleByNameOrCreate(name: string) {
+        const role = await this.rolesModel.findOne({name}, {__v: false});
+        if (!role) {
+            return this.createRole(name);
+        }
+        return role;
     }
 }
