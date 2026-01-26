3b0f66e5449d927be1ae86099fc4028e7f2f976d
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-01-26 16:26:11 +0200

    feat: implement automatic seeding for admin account

--- src/modules/database/seeds/seeder.module.ts
+++ src/modules/database/seeds/seeder.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import {SeederService} from "./seeder.service";
 import {RolesModule} from "../../roles/roles.module";
+import {UsersModule} from "../../users/users.module";
 
 @Module({
     imports: [
-        RolesModule
+        RolesModule,
+        UsersModule,
     ],
     providers: [SeederService]
 })
