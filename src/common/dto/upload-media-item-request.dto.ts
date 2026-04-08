8a7a4c187ca0f530cecab579b5efef8edbc78c0c
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-04-08 19:17:43 +0200

    feat: replace `UploadMaterialRequestDto` with `UploadMediaItemRequestDto` and add `context` property for media item type validation

--- src/common/dto/upload-media-item-request.dto.ts
+++ src/common/dto/upload-media-item-request.dto.ts
@@ -1,9 +1,15 @@
 import {FileMetadataDto} from "./file-metadata.dto";
 import {ApiProperty} from "@nestjs/swagger";
-import {ArrayMaxSize, IsArray, ValidateNested} from "class-validator";
+import {ArrayMaxSize, IsArray, IsEnum, IsNotEmpty, ValidateNested} from "class-validator";
 import {Type} from "class-transformer";
+import {MediaItemType} from "../enums/media-item-type.enum";
 
-export class UploadMaterialRequestDto {
+export class UploadMediaItemRequestDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsEnum(MediaItemType, {message: `Type must be one of ${Object.values(MediaItemType).join(', ')}`})
+    context: MediaItemType;
+
     @ApiProperty({type: [FileMetadataDto]})
     @IsArray()
     @ArrayMaxSize(10, {message: 'You cannot upload more than 10 files at once'})
