53ec1856af13fa74e9b8148a0b2b9686bef2c36b
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-04-09 13:04:41 +0200

    feat: introduce assignments module with schema, service, repository, and controller, and integrate into app module

---
+++ src/modules/assignments/assignments.repository.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Assignment} from "./schemas/assignments.schema";
+import {Model, QueryFilter} from "mongoose";
+
+@Injectable()
+export class AssignmentsRepository {
+
+    constructor(@InjectModel(Assignment.name) private readonly assignmentsModel: Model<Assignment>) {
+    }
+
+    async createAssignment(assignment: Assignment) {
+        return this.assignmentsModel.create(assignment);
+    }
+
+    async findAll(query: QueryFilter<Assignment>, select: Record<string, boolean>) {
+        return this.assignmentsModel.find(query)
+            .sort({createdAt: -1})
+            .select(select);
+    }
+
+    async findOne(query: QueryFilter<Assignment>) {
+        return this.assignmentsModel.findOne(query);
+    }
+
+    async findAssignmentOwner(query: QueryFilter<Assignment>) {
+        return this.assignmentsModel.findOne(query)
+            .select({assignmentItems: false})
+            .populate({
+                path: 'course',
+                select: 'instructor',
+                populate: {
+                    path: 'instructor',
+                    select: '_id'
+                }
+            });
+    }
+
+    async deleteAndReturn(query: QueryFilter<Assignment>) {
+        return this.assignmentsModel.findOneAndDelete(query);
+    }
+
+}
