098788037e2883dc2c8730efcbe77103623ae798
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-04-26 00:14:05 +0300

    feat: add webhook handler for updating similarity reports and processing plagiarism results

--- src/modules/similarity/similarity.service.ts
+++ src/modules/similarity/similarity.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from '@nestjs/common';
 import {InjectQueue} from "@nestjs/bullmq";
 import {Queue} from "bullmq";
+import {PlagiarismEndpointResponse} from "./dto/plagiarism-endpoint.response";
 import {SimilarityRepository} from "./similarity.repository";
+import {SimilarityStatus} from "./enums/similarity-status.enum";
+import {SimilarityResult} from "./schemas/similarity-result.schema";
+import {HttpStatusText} from "../../common/enums/http-status-text.enum";
+import {Types} from "mongoose";
 import {AssignmentSubmissionsRepository} from "../assignment-submissions/assignment-submissions.repository";
+import {AppResponseDto} from "../../common/dto/app-response.dto";
 import {SimilarityMapper} from "./similarity.mapper";
 import {AssignmentSubmissionsService} from "../assignment-submissions/assignment-submissions.service";
 import {ConfigService} from "@nestjs/config";
@@ -39,6 +45,46 @@ export class SimilarityService {
             }
         );
 
+    }
+
+    async webhookHandler(webhookRequest: PlagiarismEndpointResponse): Promise<AppResponseDto<null>> {
+        const updatedValue: { status?: SimilarityStatus, errorLog?: string, results?: SimilarityResult[] } = {};
+
+        if (webhookRequest.hasError && webhookRequest.errorMessage) {
+            updatedValue.status = SimilarityStatus.FAILED;
+            updatedValue.errorLog = webhookRequest.errorMessage;
+        } else if (!webhookRequest.hasError && webhookRequest.results) {
+            updatedValue.status = SimilarityStatus.COMPLETED;
+            updatedValue.errorLog = "";
+            updatedValue.results = webhookRequest.results?.map(res => ({
+                student: new Types.ObjectId(res.studentId),
+                submission: new Types.ObjectId(res.submissionId),
+                matches: res.matches,
+            }));
+        }
+
+        if (Object.keys(updatedValue).length > 0) {
+            await this.similarityRepository.updateSimilarityReport({
+                    assignment: new Types.ObjectId(webhookRequest.assignmentId)
+                },
+                updatedValue,
+            );
+
+            if (updatedValue.status === SimilarityStatus.COMPLETED) {
+                await this.assignmentSubmissionsService.processPlagiarismResults(
+                    new Types.ObjectId(webhookRequest.assignmentId),
+                    updatedValue.results!,
+                );
+            }
+        }
+
+        const appResponse: AppResponseDto<null> = {
+            status: HttpStatusText.SUCCESS,
+            message: 'Similarity report updated successfully',
+            data: null,
+        }
+
+        return appResponse;
     }
 
 }
